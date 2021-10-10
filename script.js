eaf604eefd90e19e538eec3097b0bb40dff97bb0
djjean <djjean90@gmail.com>
2021-10-10 23:03:21 +0300

    final

---
+++ script.js
@@ -0,0 +1,27 @@
+//Script.js
+
+const sounds = ["applause", "boo", "victory", "gasp", "wrong", "tada"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.querySelector(`#${sound}`).play();
+  });
+
+  document.querySelector("#buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.querySelector(`#${sound}`);
+
+    console.log(song);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
